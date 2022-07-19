ab2eb0e5eedb46b031cd130953d8b08ae672e00f
Paulo Sergio <pauloserg861@gmail.com>
2022-07-19 08:25:08 -0300

    adicionando estrutura de projeto

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="columns is-one-quarter">
+      <SideBar />
+    </div>
+    <div class="columns is-three-quarter">
+
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import SideBar from "./components/SideBar.vue";
+export default {
+    name: "App",
+    components: { SideBar }
+}
+</script>
+
+<style>
+  @import './assets/base.css';
+</style> 
